0e94959a66ab52d1a28e6b6601b4aaa073122779
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2021-05-19 10:08:42 +0200

    second commit

--- detection_door/src/detection_door.py
+++ detection_door/src/detection_door.py
@@ -151,100 +151,7 @@ class YOLO_np(object):
 
 
 
-class YOLO(object):
-    _defaults = default_config
 
-    @classmethod
-    def get_defaults(cls, n):
-        if n in cls._defaults:
-            return cls._defaults[n]
-        else:
-            return "Unrecognized attribute name '" + n + "'"
-
-    def __init__(self, **kwargs):
-        super(YOLO, self).__init__()
-        self.__dict__.update(self._defaults) # set up default values
-        self.__dict__.update(kwargs) # and update with user overrides
-        self.class_names = get_classes(self.classes_path)
-        self.anchors = get_anchors(self.anchors_path)
-        self.colors = get_colors(self.class_names)
-        K.set_learning_phase(0)
-        self.inference_model = self._generate_model()
-
-    def _generate_model(self):
-        '''to generate the bounding boxes'''
-        weights_path = os.path.expanduser(self.weights_path)
-        assert weights_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
-
-        # Load model, or construct model and load weights.
-        num_anchors = len(self.anchors)
-        num_classes = len(self.class_names)
-        #YOLOv3 model has 9 anchors and 3 feature layers but
-        #Tiny YOLOv3 model has 6 anchors and 2 feature layers,
-        #so we can calculate feature layers number to get model type
-        num_feature_layers = num_anchors//3
-
-        if self.model_type.startswith('scaled_yolo4_') or self.model_type.startswith('yolo5_'):
-            # Scaled-YOLOv4 & YOLOv5 entrance, enable "elim_grid_sense" by default
-            inference_model = get_yolo5_inference_model(self.model_type, self.anchors, num_classes, weights_path=weights_path, input_shape=self.model_image_size + (3,), confidence=self.score, iou_threshold=self.iou, elim_grid_sense=True)
-        elif self.model_type.startswith('yolo3_') or self.model_type.startswith('yolo4_') or \
-             self.model_type.startswith('tiny_yolo3_') or self.model_type.startswith('tiny_yolo4_'):
-            # YOLOv3 & v4 entrance
-            inference_model = get_yolo3_inference_model(self.model_type, self.anchors, num_classes, weights_path=weights_path, input_shape=self.model_image_size + (3,), confidence=self.score, iou_threshold=self.iou, elim_grid_sense=self.elim_grid_sense)
-        elif self.model_type.startswith('yolo2_') or self.model_type.startswith('tiny_yolo2_'):
-            # YOLOv2 entrance
-            inference_model = get_yolo2_inference_model(self.model_type, self.anchors, num_classes, weights_path=weights_path, input_shape=self.model_image_size + (3,), confidence=self.score, iou_threshold=self.iou, elim_grid_sense=self.elim_grid_sense)
-        else:
-            raise ValueError('Unsupported model type')
-
-        inference_model.summary()
-        return inference_model
-
-    def predict(self, image_data, image_shape):
-        out_boxes, out_scores, out_classes = self.inference_model.predict([image_data, image_shape])
-
-        out_boxes = out_boxes[0]
-        out_scores = out_scores[0]
-        out_classes = out_classes[0]
-
-        out_boxes = out_boxes.astype(np.int32)
-        out_classes = out_classes.astype(np.int32)
-        return out_boxes, out_classes, out_scores
-
-    def detect_image(self, image):
-        if self.model_image_size != (None, None):
-            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
-            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
-
-        image_data = preprocess_image(image, self.model_image_size)
-
-        # prepare origin image shape, (height, width) format
-        image_shape = np.array([image.size[1], image.size[0]])
-        image_shape = np.expand_dims(image_shape, 0)
-
-        start = time.time()
-        out_boxes, out_classes, out_scores = self.predict(image_data, image_shape)
-        end = time.time()
-        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
-        print("Inference time: {:.8f}s".format(end - start))
-
-        #draw result on input image
-        image_array = np.array(image, dtype='uint8')
-        image_array = draw_boxes(image_array, out_boxes, out_classes, out_scores, self.class_names, self.colors)
-
-        out_classnames = [self.class_names[c] for c in out_classes]
-        print("Fine detect image 2")
-        return Image.fromarray(image_array), out_boxes, out_classnames, out_scores
-
-    def dump_model_file(self, output_model_file):
-        self.inference_model.save(output_model_file)
-
-    def dump_saved_model(self, saved_model_path):
-        model = self.inference_model
-        os.makedirs(saved_model_path, exist_ok=True)
-
-        tf.keras.experimental.export_saved_model(model, saved_model_path)
-        print('export inference model to %s' % str(saved_model_path))
 
 
 
@@ -303,16 +210,9 @@ def pixel2depth(pub):
 
 
 if __name__ == '__main__':
-    path = raw_input('Inserisci il path della directory in cui è contenuto il nodo ros: ')
-    path2 = ""
-    path2 = path + "/detection_door/src"
-    print(path2)
-    os.chdir(path2)
-    print("Python version")
-    print (sys.version)
-    print("Version info.")
-    print (sys.version_info)
-    print(tf.version.VERSION)
+    path = os.path.dirname(os.path.abspath(__file__))
+    print(path)
+    os.chdir(path)
     yolo3 = YOLO_np()
     pub = rospy.Publisher("/results", String, queue_size=10)
     global images_pepper
